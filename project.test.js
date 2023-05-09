34e8989941d8f53d4c82f3141a64ad9d714354cb
ronit019u <110216859+ronit019u@users.noreply.github.com>
2023-05-09 19:17:40 +0530

    tests for all the given tasks in the project

--- project.test.js
+++ project.test.js
@@ -1,4 +1,4 @@
-const {capitalize} = require('./project');
+const {capitalize, reverseString, calculator, caesarCipher, analyzeArray} = require('./project');
 describe('capitalize', () => {
     test('capitalizes the first character of the string', () => {
         expect(capitalize('hello world')).toBe('Hello world');
@@ -14,3 +14,66 @@ describe('capitalize', () => {
     })
 
 });
+
+
+describe('reverseString', () => {
+    test('reverses the string', () => {
+        expect(reverseString('hello world')).toBe('dlrow olleh');
+    })
+    test('workds with empty string', () => {
+        expect(reverseString('')).toBe('');
+    })
+         });
+
+
+         describe('calculator', () => {
+            test("A calculator object that contains functions for the basic operations: ADD", () => {
+                expect(calculator.add(1,3)).toBe(4);
+            });
+            
+            test("A calculator object that contains functions for the basic operations: SUBTRACT", () => {
+                expect(calculator.subtract(2,1)).toBe(1);
+            });
+            
+            test("A calculator object that contains functions for the basic operations: MULTIPLY", () => {
+                expect(calculator.multiply(2,3)).toBe(6);
+            });
+            
+            test("A calculator object that contains functions for the basic operations: DIVIDE", () => {
+                expect(calculator.divide(10,2)).toBe(5);
+            })
+            });
+
+            describe('caesarCipher', () => {
+                test('caesar case', () => {
+                    expect(caesarCipher('hello world', 3)).toBe('khoor zruog')
+                })
+                test('wraps z to a', () => {
+                    expect(caesarCipher('z', 1)).toBe('a');
+                })
+                test('keep the same case of all the letters', () => {
+                    expect(caesarCipher('Hello', 3)).toBe('Khoor');
+                })
+                test('keep the punctuation', () => {
+                    expect(caesarCipher('Hello!?', 3)).toBe('Khoor!?');
+                })
+            })
+
+            describe('analyzeArray', () => {
+                test('average, minimum, maximum and length of the array', () => {
+                    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+                        average: 4,
+                        min: 1,
+                        max: 8,
+                        length: 6
+                    });
+                })
+                test('anaalyze negative array', () => {
+                    expect(analyzeArray([1, 0, -1])).toEqual({
+                        average: 0,
+                        min: -1,
+                        max: 1,
+                        length: 3
+                    })
+                })
+            })
